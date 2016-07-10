bf69208a1af39df8d3a923c284e4832581ff8461
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-10 17:18:09 +0100

    Hello world

---
+++ index.js
@@ -0,0 +1,32 @@
+var g2js = require('gradle-to-js/lib/parser');
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+var isProduction = app.get('env') === 'production';
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.text());
+app.disable('x-powered-by');
+
+app.use(function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.post("/parse/", bodyParser.text({type: '*/*'}), function(req,res){
+  g2js.parseText(req.body).then(function(representation) {
+    res.end(JSON.stringify(representation))
+  });
+});
+
+app.use(function(err, req, res, next) {
+  console.error('ERR:', err);
+  console.error('STACK:', err.stack);
+  res.status(500).send({error: 'Something went wrong.'});
+});
+
+app.listen(port, function() {
+  console.log('Listening on', port);
+});
